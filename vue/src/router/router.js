47177dd97ddee6e614ecdab1995fa96655baafd6
8hatch8 <yutaka.pg@gmail.com>
2022-04-14 15:02:30 +0900

    add: Welcomeページを作成

--- vue/src/router/router.js
+++ vue/src/router/router.js
@@ -1,10 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+import Welcome from "../views/Welcome";
 
-const routes = []
+const routes = [
+  {
+    path: "/",
+    name: "Welcome",
+    component: Welcome,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
