6c31a48bab4d6f1102e86497441bea8d41b3e0e9
8hatch8 <yutaka.pg@gmail.com>
2022-04-14 17:25:53 +0900

    add: Buttonパーツを作成

---
+++ vue/src/components/share/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="Button" @click="$emit('input', $event)">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: { type: String, required: true },
+  },
+};
+</script>
+
+<style scoped>
+.Button {
+  text-decoration: none;
+  background: #51b392;
+  color: white;
+  font-weight: bold;
+  border: 0;
+  border-radius: 3px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+</style>
