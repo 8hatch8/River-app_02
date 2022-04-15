cb6aa04d14acab7b58c99ff28f051cd3c0c2ad3e
8hatch8 <yutaka.pg@gmail.com>
2022-04-15 12:32:24 +0900

    add: フォームコンポーネントの表示切り替え機能を追加

--- vue/src/views/Welcome.vue
+++ vue/src/views/Welcome.vue
@@ -1,15 +1,34 @@
 <template>
-  <div>
-    {{ test }}
+  <div class="container welcome">
+    <div v-if="shouldShowLoginForm">
+      <login-form @show-signup-form="shouldShowLoginForm = false" />
+    </div>
+    <div v-if="!shouldShowLoginForm">
+      <signup-form @show-login-form="shouldShowLoginForm = true" />
+    </div>
   </div>
 </template>
 
 <script>
+import LoginForm from "../components/LoginForm.vue";
+import SignupForm from "../components/SignupForm.vue";
 export default {
+  components: { LoginForm, SignupForm },
   data() {
     return {
-      test: "this is test",
+      shouldShowLoginForm: true,
     };
   },
 };
 </script>
+
+<style scoped>
+.container {
+  width: 90%;
+  max-width: 960px;
+  margin: 80px auto;
+  border-radius: 15px;
+  box-shadow: 2px 4px 6px rgba(28, 6, 49, 0.1);
+  background: white;
+}
+</style>
