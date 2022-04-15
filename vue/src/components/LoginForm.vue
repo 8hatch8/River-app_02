baa3c8f8778c14656adef5488df97789fe063d60
8hatch8 <yutaka.pg@gmail.com>
2022-04-15 12:33:04 +0900

    add: ログイン・サインアップコンポーネントを追加

---
+++ vue/src/components/LoginForm.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="login-form">
+    <div class="title">
+      <h1 class="font font-lg">
+        River for <br />
+        Online Meeting
+      </h1>
+    </div>
+    <div class="subtitle">
+      <h2 class="font-md">ログイン</h2>
+      <p class="font-sm">みんなの意見が会議をかえる</p>
+    </div>
+    <form class="Form" @submit.prevent="login">
+      <input-box
+        id="email"
+        label="メールアドレス"
+        name="email"
+        type="text"
+        placeholder="メールアドレス"
+        :required="true"
+        :value="form.email"
+        @input="form.email = $event.target.value"
+      />
+      <input-box
+        id="password"
+        label="パスワード"
+        name="password"
+        type="password"
+        placeholder="パスワード（6文字以上）"
+        :required="true"
+        :value="form.password"
+        @input="form.password = $event.target.value"
+      />
+      <div class="error">{{ error }}</div>
+      <form-button label="ログインする" />
+    </form>
+    <p>
+      はじめての方は
+      <span class="underline" @click="showSignupForm">こちら</span>
+    </p>
+  </div>
+</template>
+
+<script>
+// import axios from "axios";
+import InputBox from "../components/share/InputBox.vue";
+import FormButton from "../components/share/Button.vue";
+export default {
+  components: { InputBox, FormButton },
+  data() {
+    return {
+      form: {
+        email: "",
+        password: "",
+      },
+      error: null,
+    };
+  },
+  methods: {
+    showSignupForm() {
+      this.$emit("show-signup-form");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.login-form {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  text-align: center;
+  margin: auto;
+  padding: 30px;
+}
+.title,
+.subtitle {
+  padding: 10px 0;
+}
+.Form {
+  width: 80%;
+  max-width: 400px;
+}
+.Button {
+  margin: 30px 0;
+}
+</style>
---
+++ vue/src/components/SignupForm.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="login-form">
+    <div class="title">
+      <h1 class="font font-lg">
+        River for <br />
+        Online Meeting
+      </h1>
+    </div>
+    <div class="subtitle">
+      <h2 class="font-md">新規登録</h2>
+      <p class="font-sm">みんなの意見が会議をかえる</p>
+    </div>
+    <form class="Form" @submit.prevent="signUp">
+      <input-box
+        id="nickname"
+        label="お名前"
+        name="nickname"
+        type="text"
+        placeholder="お名前"
+        :required="true"
+        :value="form.nickname"
+        @input="form.nickname = $event.target.value"
+      />
+      <input-box
+        id="email"
+        label="メールアドレス"
+        name="email"
+        type="text"
+        placeholder="メールアドレス"
+        :required="true"
+        :value="form.email"
+        @input="form.email = $event.target.value"
+      />
+      <input-box
+        id="password"
+        label="パスワード"
+        name="password"
+        type="password"
+        placeholder="パスワード（6文字以上）"
+        :required="true"
+        :value="form.password"
+        @input="form.password = $event.target.value"
+      />
+      <input-box
+        id="password-confirmation"
+        label="パスワード（確認用）"
+        name="password_confirmation"
+        type="password"
+        placeholder="パスワード（確認用）"
+        :required="true"
+        :value="form.passwordConfirmation"
+        @input="form.passwordConfirmation = $event.target.value"
+      />
+      <div class="error">{{ error }}</div>
+      <form-button label="登録する" />
+    </form>
+    <p>
+      登録済みの方は
+      <span class="underline" @click="showLoginForm">こちら</span>
+    </p>
+  </div>
+</template>
+
+<script>
+// import axios from "axios";
+import InputBox from "../components/share/InputBox.vue";
+import FormButton from "../components/share/Button.vue";
+export default {
+  components: { InputBox, FormButton },
+  data() {
+    return {
+      form: {
+        nickname: "",
+        email: "",
+        password: "",
+        passwordConfirmation: "",
+      },
+      error: null,
+    };
+  },
+  methods: {
+    showLoginForm() {
+      this.$emit("show-login-form");
+    },
+    async signUp() {},
+  },
+};
+</script>
+
+<style scoped>
+.login-form {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  text-align: center;
+  margin: auto;
+  padding: 30px;
+}
+.title,
+.subtitle {
+  padding: 10px 0;
+}
+.Form {
+  width: 80%;
+  max-width: 400px;
+}
+.Button {
+  margin: 30px 0;
+}
+</style>
