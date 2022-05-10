f6f9f67fbae7f4d41a987553210ac6bac7050e83
8hatch8 <yutaka.pg@gmail.com>
2022-05-10 13:50:30 +0900

    add: roomモデルを作成

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_13_065709) do
+ActiveRecord::Schema.define(version: 2022_05_10_042858) do
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "password_digest"
+    t.text "agendas_order"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -26,4 +36,5 @@ ActiveRecord::Schema.define(version: 2022_04_13_065709) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "rooms", "users"
 end
