94fdbb0423385eeb867885aab9d8e2c3edb186b2
8hatch8 <yutaka.pg@gmail.com>
2022-05-21 13:17:40 +0900

    update: corsのURLを変更

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://river-test2-37275'
+    origins 'https://river-test2-37275.herokuapp.com'
 
     resource '*',
              headers: :any,
