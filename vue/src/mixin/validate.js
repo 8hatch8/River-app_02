50c1f43aa853e836215bf6a0b9e56972950a4663
8hatch8 <yutaka.pg@gmail.com>
2022-05-23 15:05:14 +0900

    update: @をつかった相対パスに変更

--- vue/src/mixin/validate.js
+++ vue/src/mixin/validate.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { removeItem, axiosHeaders, apiServer } from "./auth";
+import { removeItem, axiosHeaders, apiServer } from "@/mixin/auth";
 import { ref } from "vue";
 const error = ref(null);
 
