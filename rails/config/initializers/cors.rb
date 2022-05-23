e69e1a387a9e495f490ac409c709102d831b7c90
8hatch8 <yutaka.pg@gmail.com>
2022-05-23 12:49:19 +0900

    fix: 環境変数のprefixつけわすれを修正

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['RIVER_APP_URL']
+    origins ENV['VUE_APP_RIVER_APP_URL']
 
     resource '*',
              headers: :any,
