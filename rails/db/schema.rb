de97ebb75198944c90a53a9c57c2aa78a36b45bb
8hatch8 <yutaka.pg@gmail.com>
2022-05-26 00:03:06 +0900

    update: imageカラムをstring -> text型へ変更

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_11_060411) do
+ActiveRecord::Schema.define(version: 2022_05_25_132030) do
 
   create_table "agendas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 2022_05_11_060411) do
     t.string "uid", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname"
-    t.string "image"
+    t.text "image"
     t.string "email"
     t.text "tokens"
     t.datetime "created_at", precision: 6, null: false
