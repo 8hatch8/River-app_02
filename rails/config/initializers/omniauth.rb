d12d8997f34712c54ea122286c6266efd5a7790f
8hatch8 <yutaka.pg@gmail.com>
2022-05-25 17:09:15 +0900

    add: googleのスコープを設定

--- rails/config/initializers/omniauth.rb
+++ rails/config/initializers/omniauth.rb
@@ -1,5 +1,14 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET_KEY']
   provider :facebook, ENV['FACEBOOK_API_KEY'], ENV['FACEBOOK_API_SECRET_KEY']
-  provider :google_oauth2, ENV['GOOGLE_API_KEY'], ENV['GOOGLE_API_SECRET_KEY']
+  provider :google_oauth2,
+           ENV['GOOGLE_API_KEY'],
+           ENV['GOOGLE_API_SECRET_KEY'],
+           {
+             scope: 'userinfo.email, userinfo.profile',
+             image_aspect_ratio: 'square',
+             image_size: 50,
+           }
 end
+OmniAuth.config.allowed_request_methods = %i[get]
+OmniAuth.config.silence_get_warning = true
