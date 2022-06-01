0f5289c14fde10143f4fc4a046357f59e0f877c8
8hatch8 <yutaka.pg@gmail.com>
2022-06-02 02:09:42 +0900

    add: チャットフォームの高さを自動調節

--- vue/src/components/Chatroom/ChatForm.vue
+++ vue/src/components/Chatroom/ChatForm.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="chatform-wrapper">
-    <form>
+    <form class="form">
+      <div class="dummy" aria-hidden="true">{{ text + " " }}</div>
       <textarea
+        class="textarea"
+        id="chatform"
         name="chat"
         cols="30"
         placeholder="メッセージを入力してshift+Enterで送信してください"
@@ -36,17 +39,40 @@ export default {
 <style scoped lang="scss">
 .chatform-wrapper {
   z-index: 10;
-}
-textarea {
-  resize: none;
-  font-size: 1rem;
-  min-width: 100%;
-  height: 60px;
-  padding: 10px 15px;
-  border-radius: 10px;
-  outline: none;
-  &:focus {
-    background: rgb(226, 239, 250);
+  .form {
+    position: relative;
+    font-size: 1rem;
+    line-height: 1.8;
+    .dummy,
+    .textarea {
+      overflow: hidden;
+      padding: 5px 15px;
+      white-space: pre-wrap;
+      word-wrap: break-word;
+      overflow-wrap: break-word;
+      &.dummy {
+        visibility: hidden;
+        min-height: 60px;
+        border: 1px solid;
+      }
+      &.textarea {
+        position: absolute;
+        top: 0;
+        left: 0;
+        display: block;
+        width: 100%;
+        height: 100%;
+        border: 1px solid #b6c3c6;
+        border-radius: 10px;
+        outline: none;
+        resize: none;
+        &:focus {
+          background: rgb(226, 239, 250);
+          box-shadow: 0 0 0 4px rgba(255, 255, 255, 0.3);
+          outline: 0;
+        }
+      }
+    }
   }
 }
 </style>
